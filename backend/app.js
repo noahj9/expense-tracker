99e07a9f959aedf16b9e37962a4c1f602e7fd34f
Noah Jina <109239448+noahj9@users.noreply.github.com>
2023-06-20 18:24:07 -0600

    server creation

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const { config } = require('dotenv')
+
+const app = express()
 require ('dotenv').config()
 
 const PORT = process.env.PORT
 
+// MIDDLEWARES
+app.use(express.json())
+app.use(cors({}))
+
+app.get('/', (req, res)=> {
+    res.send('Hello World')
+})
+
 const server = () => {
-    console.log('You are listening to port:', PORT);
+    app.listen(PORT, () => {
+        console.log("listening to port:", PORT)
+    })
 }
 
 server()
